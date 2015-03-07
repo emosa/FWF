45472900e5f5874c3021629e85c7c4833212b0c2
Elimarie <iglitzi@fullsail.edu>
2015-03-06 21:53:07 -0500

    Assignment
    Grocery list

--- W1/js/app.js
+++ W1/js/app.js
@@ -1,3 +1,23 @@
-function DBController($scope){
-	$scope.userName;
-	}
+var app = angular.module('appList',[]);
+	
+	app.controller('GroceryList', ['$scope', function($scope){
+	
+	$scope.items = [
+		
+			{'name':'Sofrito','done':false},
+			{'name':'Sazon','done':false},
+			{'name':'Chicken','done':false},
+			{'name':'Bread','done':false},
+			{'name':'Tomatoes','done':false}
+];
+	
+	$scope.addItem = function(){
+		$scope.items.push({name: $scope.item, done:false});
+		$scope.item = "";
+	};
+	
+	$scope.deleteItem = function(index){
+    	$scope.items.splice(index, 1);
+  	}	
+	
+}]);
